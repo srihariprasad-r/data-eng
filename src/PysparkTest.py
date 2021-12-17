20e36fabb39d5c60ae021a5fe4b84d0864064e51
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2021-12-17 19:49:20 +0530

    removed files

--- src/PysparkTest.py
+++
@@ -1,5 +0,0 @@
-from pyspark.sql import SparkSession
-
-sparkSession = SparkSession.builder.appName("test").getOrCreate()
-
-sparkSession.createDataFrame([(1, "value1"), (2, "value2")], ["id", "value"]).show()
