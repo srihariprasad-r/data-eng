2dde4559f0907967d4cb0068426ca2f6916b538d
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2021-12-17 19:43:29 +0530

    added json data analysis

--- src/readjson.py
+++ src/readjson.py
@@ -1,9 +1,21 @@
-from pyspark.sql import SparkSession
+from pyspark.sql import SparkSession, functions as F
 from pyspark.sql.types import StructType, StructField, StringType
+from urllib.request import urlopen
 
-sparkSession = SparkSession.builder.appName("test").getOrCreate()
+spark = SparkSession.builder.appName("test").getOrCreate()
 
-multiline_df = sparkSession.read.option("multiline","true") \
+# json file as input
+multiline_df = spark.read.option("multiline","true") \
     .json("inputFiles/jsoninput.json")
-multiline_df.show()
+# multiline_df.show()
+# spark.read.json("inputFiles/jsoninput.json").explain()
 
+# online json processing
+urlJsonData = 'https://randomuser.me/api/0.8/?results=10'
+httpData = urlopen(urlJsonData).read().decode('utf-8')
+rdd = spark.sparkContext.parallelize([httpData])
+df = spark.read.json(rdd)
+# explode array and extract nested columns
+df.withColumn('expr_result', F.explode(df["results"])).select(F.col("expr_result.user.cell").alias('mobile phone'),
+                                                              F.col("expr_result.user.location.city").alias('city')) \
+    .show()
